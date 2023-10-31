8c5d65d0583966705d476125ae04bd4089d7fe23
ycangurkok <ycangurkok@gmail.com>
2023-11-01 01:14:06 +0300

    Photo functionality added, my branch

--- App.js
+++ App.js
@@ -1,28 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, SafeAreaView } from 'react-native';
+import { useState, useRef } from 'react';
+import { Camera } from 'expo-camera';
 
 export default function App() {
+  let cameraRef = useRef();
+  const [photo, setPhoto] = useState();
+
+
+  let takePic = async () => {
+    let options = {
+      quality: 1,
+      base64: true,
+      exif: false
+    };
+
+    let newPhoto = await cameraRef.current.takePictureAsync(options);
+    setPhoto(newPhoto);
+  };
+
+  if (photo) {
+    let sharePic = async () => {
+      console.log(photo.base64);
+    };
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <Image style={styles.preview} source={{ uri: "data:image/jpg;base64," + photo.base64 }} />
+        <Button title="Send to EP" onPress={sharePic} />
+        <Button title="Discard" onPress={() => setPhoto(undefined)} />
+      </SafeAreaView>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <View style={{gap:50}}>
-        <Text>Hello world!</Text>
-        <Button
-          onPress={() => {
-            console.log('You tapped the button!');
-          }}
-          title="Camera"
-        />
-        <StatusBar style="auto" />
+    <Camera style={styles.container} ref={cameraRef}>
+      <View style={styles.buttonContainer}>
+        <Button title="Take Pic" onPress={takePic} />
       </View>
-    </View>
+      <StatusBar style="auto" />
+    </Camera>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    backgroundColor: '#fff',
+    alignSelf: 'flex-end'
+  },
+  preview: {
+    alignSelf: 'stretch',
+    flex: 1
+  }
 });
