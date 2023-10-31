e1a2eedbb9ac93e625632fb8f93dd54de112a49c
ycangurkok <ycangurkok@gmail.com>
2023-10-31 22:02:10 +0300

    Base app initialization

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{gap:50}}>
+        <Text>Hello world!</Text>
+        <Button
+          onPress={() => {
+            console.log('You tapped the button!');
+          }}
+          title="Camera"
+        />
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
