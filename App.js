0680665263c0c5e23d21f08b4ec11984d740ef24
bkulek20 <bkulek20@ku.edu.tr>
2023-11-04 10:26:50 +0300

    camera button added

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
 import * as MediaLibrary from 'expo-media-library';
 import Button from './src/components/Button';
@@ -10,6 +10,7 @@ export default function App() {
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
   const cameraRef = useRef(null); // Use useRef to create a ref
+  const [isCameraOpen, setIsCameraOpen] = useState(false); // Track whether the camera is open
 
   let options = {
     quality: 1,
@@ -17,11 +18,24 @@ export default function App() {
     exif: false
   };
 
-  useEffect(() => {
+  /*useEffect(() => {
     (async () => {
       MediaLibrary.requestPermissionsAsync();
       const cameraStatus = await Camera.requestCameraPermissionsAsync();
       setCameraPermission(cameraStatus.status === 'granted');
+    })();
+  }, []);*/
+
+  useEffect(() => {
+    (async () => {
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      const mediaLibraryStatus = await MediaLibrary.requestPermissionsAsync();
+
+      if (cameraStatus.status === 'granted' && mediaLibraryStatus.status === 'granted') {
+        setCameraPermission(true);
+      } else {
+        setCameraPermission(false);
+      }
     })();
   }, []);
 
@@ -40,7 +54,7 @@ export default function App() {
   const saveImage = async () => { //backende yollama kodu gelicek
     if(image) {
       try{
-        let b64 = photo.base64;
+        let b64 = image.base64;
         console.log(b64)
       } catch(e) {
         console.log(e);
@@ -48,12 +62,23 @@ export default function App() {
     }
   }
 
+  const openCamera = () => {
+    setIsCameraOpen(true);
+  };
+
+  const closeCamera = () => {
+    setIsCameraOpen(false);
+  };
+
+
   if (cameraPermission === false) {
     return <Text>No access to the camera</Text>;
   }
 
-  return (
-    <View style={styles.container}>
+  if (isCameraOpen) {
+    // Render the camera view
+    return (
+      <View style={styles.container}>
       {!image ? (
         <Camera
           style={styles.camera}
@@ -64,7 +89,7 @@ export default function App() {
           <View style={{
             flexDirection: 'row',
             justifyContent: 'space-between',
-            padding: 60,
+            padding: 30,
           }}>
             <Button icon={'retweet'} onPress={() => {
               setType(type === CameraType.back ? CameraType.front : CameraType.back)
@@ -72,7 +97,10 @@ export default function App() {
             <Button icon={'flash'} 
               color={flash === 'off' ? 'gray' : '#f1f1f1'}
               onPress={() => {
-                setFlash(flash === 'off' ? 'on' : 'off')
+                setFlash(flash === 'off' 
+                  ? 'on'
+                  : 'off'
+                  )
               }}/>
           </View>
         </Camera>
@@ -91,10 +119,28 @@ export default function App() {
         </View>
         :
         <Button title={'Take a picture'} icon="camera" onPress={takePicture} />
+        
         }
+        <TouchableOpacity onPress={closeCamera}>
+          <Text style={{ fontSize: 18, color: 'white', textAlign: 'center', backgroundColor: 'blue', padding: 10 }}>
+            Back to Home Screen
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
+} else {
+    // Render the home page with the camera button
+    return (
+      <View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}>
+        <TouchableOpacity onPress={openCamera}>
+          <View style={{ borderColor: 'black', borderWidth: 1, padding: 10, borderRadius: 5 }}>
+            <Text style={{ fontSize: 24, color: 'black' }}>Camera</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
